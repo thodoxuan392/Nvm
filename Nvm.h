412fefd17a3544a7be705a5b2c0bd2dd801493d6
Xuan Tho Do <tho.dx@novaintechs.com>
2025-03-19 00:02:08 +0700

    feat: update Nvm interface

--- Nvm.h
+++ Nvm.h
@@ -7,19 +7,6 @@
 
 #include <Nvm_Cfg.h>
 
-typedef uint16_t NVM_BlockId;
-
-typedef struct
-{
-	uint16_t pageId;
-	uint16_t byteOffset;
-	uint16_t byteLength;
-	void* romData;
-	void* ramData;
-	bool readAll;
-	bool writeAll;
-} NVM_BlockDescriptor;
-
 void NVM_Init(void);
 void NVM_Deinit(void);
 void NVM_Loop(void);
